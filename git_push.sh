5d651c6ff6ee594643572966bb24099401161d1b
romepeng <romepeng@outlook.com>
2022-05-21 10:00:28 +0800

    change git_push

--- git_push.sh
+++ git_push.sh
@@ -5,8 +5,8 @@ cd ~/rrdata
 git add .
 read -p "input commit -m context: " context
 
-echo $(context)
-git commit -m "$(context)"
+echo $context
+git commit -m "$context"
 
 
 git push -u origin master
